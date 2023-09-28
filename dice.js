43e30e7130297e17dd6bffc09b914c6fe770f98e
randellJE <randellokelly@gmail.com>
2023-09-27 22:01:19 -0400

    new branch stuff

--- dice.js
+++ dice.js
@@ -4,19 +4,21 @@ function diceRoll(num,num2,Name1) {
 }
 
 
-function d6() {
-    console.log("button clicked");
-    console.log("this is the d6 dice");
-    numberMinMax(1,6) // calls numberMinMax funstion defined below
-    document.getElementById("roll6").innerHTML=numberMinMax(1,6)
-}
-function d8() {
-    console.log("button clicked");
-    console.log("this is the d8 dice");
-    numberMinMax(1,8) // calls numberMinMax funstion defined below
-    document.getElementById("roll8").innerHTML=numberMinMax(1,8)
+// function d6() {
+//     console.log("button clicked");
+//     console.log("this is the d6 dice");
+//     numberMinMax(1,6) // calls numberMinMax funstion defined below
+//     document.getElementById("roll6").innerHTML=numberMinMax(1,6)
+// }
+// function d8() {
+//     console.log("button clicked");
+//     console.log("this is the d8 dice");
+//     numberMinMax(1,8) // calls numberMinMax funstion defined below
+//     document.getElementById("roll8").innerHTML=numberMinMax(1,8)  
+//  these two functions are repeats of line1,4 and 21,24
+// }
 
-}
+
 function numberMinMax(min,max) {
     const diceRoll=Math.floor(Math.random() * (max - min + 1)) + min;
     console.log(diceRoll);
